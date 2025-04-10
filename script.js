d8a96567877453816d0b5957e793bdc98260fdee
Amirtha1205 <144202718+Amirtha1205@users.noreply.github.com>
2025-04-10 13:31:03 +0530

    script.js

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ async function getWeather() {
         return;
     }
 
-    const url = `https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}&units=metric`;
 
     try {
         const response = await fetch(url);
